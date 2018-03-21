8193f8047b8e6b9c90f00702333cbe576cdea2f1
Atticus Pomerantz <rapomerantz@gmail.com>
2018-03-21 14:25:09 -0500

    first lecture commit

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,5 +3,12 @@ let myApp = angular.module('myApp', []);
 myApp.controller('WelcomeController', function(){
     let self = this;
     self.greeting = 'Hello!';
+    self.greet = function (name) {
+        console.log('Hello', name); 
+    }
+    self.cohort = 'you'; 
+    let instructorArray = [{name: 'Chris', pet: 'Charlie'}, {name: 'Dane', pet: 'Juniper'}]
+    self.instructors = instructorArray; 
   });
   
+  //if you have a variable but it's not linked to self. it is NOT avaiable to Angular
